d171c64cf5e450f9ac9ff48c92fe1620b8addcea
progwruk <wmeshram@gmail.com>
2022-08-28 19:06:26 +0530

    [Wrukshali] Add: Fibonacci Series

--- LogicalProgram/Program.cs
+++ LogicalProgram/Program.cs
@@ -6,6 +6,21 @@ namespace LogicalProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to logical programming");
+            while (true)
+            { 
+                Console.WriteLine("Select program\n 1. fibonacci series");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        FibonacciSeries fibonacciSeries = new FibonacciSeries();
+                        fibonacciSeries.Series();
+                        break;
+                    default:
+                        Console.WriteLine("Select correct option");
+                        break;
+                }
+            }
         }
     }
 }
