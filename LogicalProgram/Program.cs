7eaf303c339319028c16332d76e3ab5763bb4dba
progwruk <wmeshram@gmail.com>
2022-08-28 18:19:48 +0530

    [Wrukshali] Add: Welcome to logical programming

--- LogicalProgram/Program.cs
+++ LogicalProgram/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace LogicalProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to logical programming");
+        }
+    }
+}
